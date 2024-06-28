82f1e53a88b4f36fbc9a2789fa874d55838f794b
carpicoder <webcarpi@gmail.com>
2024-06-28 02:00:06 +0200

    Clase 1

---
+++ Clase 1/js/main.js
@@ -0,0 +1,58 @@
+// comentario de una línea
+
+/* 
+    comentario
+    de varias
+    líneas
+*/
+
+//! rojo
+//? azul
+//todo naranja
+//* verde clarito
+
+// Declarar una variable
+let nombre;
+// Asignar un valor a la variable
+nombre = "Matias"; //! string = cadena de caracteres
+
+// Declarar y asignar un valor a una variable
+let apellido = "Coletta";
+
+const anioNacimiento = 1995;
+// anioNacimiento = 1996; //! Incorrecto!! No se pueden reasignar valores de constantes
+
+
+//? ******************************************
+//? ******************************************
+//? ******************************************
+
+let num1 = 5;
+let num2 = 3;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+// console.log(suma);
+
+let nombreCompleto = nombre + " " + apellido;
+
+// console.log("Hola, soy " + nombreCompleto + ". Mi año de nacimiento es: " + anioNacimiento + ".");
+
+// let nombreUsuario = prompt("Ingresá tu nombre.");
+// console.log("El nombre del usuario es: " + nombreUsuario);
+
+// let numero1 = parseInt(prompt("Ingresá el primer número"));
+// let numero2 = parseInt(prompt("Ingresá el segundo número"));
+
+// // console.log(numero1 + numero2);
+
+// alert("La suma es: " + (numero1 + numero2));
+
+
+// let entrada = prompt("Ingrese una letra");
+// let salida = "Letra " + entrada + " ingresada.";
+// alert(salida);
+
+console.log( Number("5.5") );
