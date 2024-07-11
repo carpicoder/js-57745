d9dfd68202e3dde278f18a6a610533b5eec737cf
carpicoder <webcarpi@gmail.com>
2024-07-12 01:55:24 +0200

    Clase 4

---
+++ Clase 4/js/main.js
@@ -0,0 +1,39 @@
+
+// function sumar(num1, num2) {
+//     return num1 + num2;
+// }
+
+// console.log(sumar(1, 2));
+
+//! funciones declaradas
+//? function nombre()
+
+// function saludar() {
+//     let usuario = "Alejandro";
+//     console.log(usuario);
+// }
+
+// saludar();
+// console.log(usuario);
+
+//! funciones anónimas
+// const suma = function (a, b) {
+//     console.log(a + b)
+// }
+
+
+//! funciones flecha
+//? si hay un solo parámetro, podemos omitir sus paréntesis
+//? si hay una sola sentencia, podemos omitir las llaves
+//? si no hay llaves, siempre retorna lo que viene después de la flecha
+//! y NO hay que usar return si sacamos llaves
+
+// const saludar = usuario => console.log("Hola, " + usuario);
+
+// saludar("Carpi");
+
+// const saludar = usuario => {
+//     return usuario;
+// };
+
+// console.log( saludar("Carpi") );
