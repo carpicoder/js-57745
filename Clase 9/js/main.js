20a94abecdcdfe8a30c31396721517d300ad0d31
carpicoder <webcarpi@gmail.com>
2024-07-31 02:38:35 +0200

    Clase 9

---
+++ Clase 9/js/main.js
@@ -0,0 +1,56 @@
+// console.log(document.body);
+
+
+// let titulo = document.getElementById("titulo");
+// console.log(titulo);
+
+// let parrafos = document.getElementsByClassName("parrafo");
+// console.log(parrafos);
+
+// let parrafos = document.getElementsByTagName("p");
+// console.log(parrafos);
+
+
+
+// let titulo = document.querySelector("#titulo");
+// console.log(titulo);
+
+// let parrafos = document.querySelectorAll(".parrafo");
+// console.log(parrafos);
+
+//! Modificar nodos
+
+// const numero = 1;
+
+// let titulo = document.querySelector(".titulo");
+// console.log(titulo.innerText);
+
+// console.log(titulo.innerHTML);
+// titulo.innerHTML = "<strong>Carpi</strong><u>Coder</u>";
+
+// titulo.className = "rojo lucas";
+
+//! Agregar nodos o elementos
+
+// const productos = ["Sillón", "Silla", "Mesa", "Ventana"]
+
+// let lista = document.createElement("ul");
+// lista.className = "lista";
+
+// productos.forEach(producto => {
+//     lista.innerHTML += `<li class="item">${producto}</li>`;
+// })
+
+// let contenedorLista = document.querySelector("#contenedor-lista");
+// contenedorLista.append(lista);
+
+
+// titulo.remove();
+
+// let numero = 1;
+
+// const string1 = "El número es: " + numero + ". Gracias.";
+// const string2 = 'El número es: ' + numero + '.Gracias.';
+// const string3 = `El número es: ${numero}. Gracias.`;
+
+// console.log(string3);
