9ac330341fe0a47f19a53827d17267d545fba18a
carpicoder <webcarpi@gmail.com>
2024-08-14 01:56:47 +0200

    Clase 13

---
+++ Clase 13/js/main.js
@@ -0,0 +1,37 @@
+
+// const nombres = ["Juan", 3, {nombre: "Octavio"}, "Emilia"];
+
+// const numeros = [12, 15, 1, 3, 50];
+
+// console.log( Math.max(...numeros) )
+
+
+// const nombres1 = ["Juan", "Guido", "Victor", "Pablo"];
+// const nombres2 = ["José", "Walter"];
+
+// const nombres = [...nombres1, "Lucas", ...nombres2]
+
+
+// console.log(nombres);
+
+
+// const usuario1 = {
+//     nombre: "Carpi",
+//     edad: 29
+// }
+
+// const usuario2 = {...usuario1, pais: "Argentina"};
+// usuario2.edad = 30;
+
+// console.log(usuario1);
+// console.log(usuario2);
+
+
+// function sumarTodosLosNumeros(...numeros) {
+//     const resultado = numeros.reduce((acc, num) => acc + num, 0);
+//     console.log(resultado);
+// }
+
+// sumarTodosLosNumeros(50, 5, 10, 90);
+// sumarTodosLosNumeros(50, 5, 10);
+// sumarTodosLosNumeros(50, 5, 10, 50, 5, 10, 50, 5, 10);
